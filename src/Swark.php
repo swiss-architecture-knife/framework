f290ecccd7cdb1a0ea7d98fd71d42fdec5ee49e6
schakko <359812+schakko@users.noreply.github.com>
2025-03-23 13:23:58 +0000

    Update version to v0.2.0 / 93e8b75

--- src/Swark.php
+++ src/Swark.php
@@ -2,6 +2,6 @@
 namespace Swark;
 
 class Swark {
-    const string VERSION = '0.1.6';
-    const string SHORT_HASH = '9367a1e';
+    const string VERSION = '0.2.0';
+    const string SHORT_HASH = '93e8b75';
 }
